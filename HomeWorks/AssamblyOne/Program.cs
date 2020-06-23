90dc6702f150b944027eb6045505dd2dddf56581
StanislauLabanau <stanislau.labanau@gmail.com>
2020-06-23 20:03:53 +0300

    hw_07 task1, task2

---
+++ HomeWorks/AssamblyOne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AssamblyOne
+{
+    class EnduroMotorcycle : Motorcycle
+    {
+        void SomeMetod()
+        {
+            _vinNumberProtected = "vin number enduro";
+            StartEngineProtected();
+
+            _vinNumberPrivateProtected = "vin number enduro";
+            StartEnginePrivateProtected();
+
+            _vinNumberPublic = "vin number enduro";
+            StartEnginePublic();
+
+            _vinNumberInternal = "vin number enduro";
+            StartEngineInternal();
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle firstMotorcycle = new Motorcycle();
+
+            firstMotorcycle._vinNumberInternal = "vin number";
+            firstMotorcycle.StartEngineInternal();
+
+            firstMotorcycle._vinNumberProtectedInternal = "vin number";
+            firstMotorcycle.StartEngineProtectedInternal();
+
+            firstMotorcycle._vinNumberPublic = "vin mumber";
+            firstMotorcycle.StartEnginePublic();
+
+            Console.Read();
+        }
+    }
+}
---
+++ HomeWorks/StringLetterCounting/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace StringLetterCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string ");
+            string someString = Console.ReadLine();
+
+            Console.WriteLine($"The result string is \n{GetSortedString(someString)}");
+
+            Console.ReadKey();
+        }
+        static StringBuilder GetSortedString(string someString)
+        {
+            const char firstLetter = 'a';
+            const char lastLetter = 'z';
+            char min;
+            int counter;
+
+            someString = someString.ToLower();
+
+            StringBuilder sb = new StringBuilder(someString);
+            StringBuilder resultSb = new StringBuilder();
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] < firstLetter || sb[i] > lastLetter)
+                {
+                    sb.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            while (sb.Length != 0)
+            {
+                counter = 0;
+                min = lastLetter;
+                for (int i = 0; i < sb.Length; i++)
+                {
+                    if (sb[i] <= min)
+                    {
+                        min = sb[i];
+                    }
+                }
+                counter = 0;
+                counter = sb.Length - sb.Replace(min.ToString(), "").Length;
+                resultSb.Append(min);
+                resultSb.Append(counter);
+            }
+
+            return resultSb;
+        }
+    }
+}
